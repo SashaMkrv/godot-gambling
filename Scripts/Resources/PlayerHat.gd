e660bfd94cb0d3c7b77976fa342ea98e25346c71
sashaMKRV <no@no.co>
2022-03-10 19:55:54 -0500

    add possible future invenotry management

--- Scripts/Resources/PlayerHat.gd
+++ Scripts/Resources/PlayerHat.gd
@@ -13,3 +13,23 @@ func change_hat(newHat: int):
 		emit_signal("hat_changed", newHat)
 		return
 	print("tried to change into hat player does not own: " + String(newHat))
+
+
+func add_hat(newHat: int):
+	if playerHasHats.has(newHat):
+		return
+	if newHat < 0:
+		print("tried to add negahat: " + String(newHat))
+	playerHasHats.append(newHat)
+
+
+# should add a method for getting the next and previous hats in the players inventory
+# and this can be done with shops too.
+# just need to make a unique resource, export the hat list for those,
+# and then attach it to the buttons and hat image for a store's ui
+# cool.
+# only, this can't be a player hat class then
+# just... a hat stock class
+# hat holder
+# the hat keeper
+# to go with hat consumers
